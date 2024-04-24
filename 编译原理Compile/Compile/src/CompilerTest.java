019c3991b896bd3c9b135e9ea6024d0361619642
kunn-hub <1284643021@qq.com>
2024-04-24 15:18:31 +0800

    comparing to the first commit, optimize the code input stream

--- 编译原理Compile/Compile/src/CompilerTest.java
+++ 编译原理Compile/Compile/src/CompilerTest.java
@@ -1,10 +1,21 @@
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CompilerTest {
     public static void main(String[] args) {
         Compiler c = new Compiler();
 
-        String str = new String("void main ( ) { int sum1 = 1 ; int sum2 = 2 ; if ( a >= b ) a = a ++ ; }");
-        String[] strArr = c.strToArr(str);
-        c.analysis(strArr);
-
+        String inputCodePath = "C:\\Code\\Java\\HeiMa_java\\Compiler\\inputCode1.txt";
+        /*
+        使用文件读取,将文本内容读取后返回字符窜
+         */
+        try {
+            String content = Files.readString(Paths.get(inputCodePath));
+            String[] strArr = c.strToArr(content);
+            c.analysis(strArr);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
